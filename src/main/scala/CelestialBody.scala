a81b5cc8190718c5750d0fa0b67cdf6e742e42f5
danielmichaeli <daniel.michaeli@aalto.fi>
2023-02-12 19:08:10 +0200

    added skeleton class CelestialBody and extensions Sun, Planet and Satellite

--- src/main/scala/CelestialBody.scala
+++ src/main/scala/CelestialBody.scala
@@ -1,5 +1,22 @@
 // THE ABSTRACT SUPERCLASS CELESTIALBODY, AS WELL AS ITS SUB-CLASSES SUN, PLANET AND SATELLITE
+import scalafx.scene.paint.Color
 
-class CelestialBody {
+//mass: kg, radius: m
+abstract class CelestialBody(val name: String, val radius: Double, val mass: Double, var xPos: Double, var yPos: Double, var xVel: Double, var yVel: Double, val color: scalafx.scene.paint.Color):
+  var distanceToSun = ??? //distance between the planet's center of mass and the sun's
+  var orbit = Vector[(Double, Double)]() //stores xPos and yPos pairs through time for drawing trajectories
+  var xAcc: Double = 0 //will be immediately recalculated with total force acting on the body
+  var yAcc: Double = 0
 
-}
+
+
+class Sun(val name: String, val radius: Double, val mass: Double, var xPos: Double, var yPos: Double, val xVel: Double, val yVel: Double, val color: scalafx.scene.paint.Color) extends CelestialBody(name, radius, mass, xPos, yPos, xVel, yVel, color):
+
+
+class Planet(val name: String, val radius: Double, val mass: Double, var xPos: Double, var yPos: Double, val xVel: Double, val yVel: Double, val color: scalafx.scene.paint.Color) extends CelestialBody(name, radius, mass, xPos, yPos, xVel, yVel, color):
+
+
+class Satellite(val name: String, val radius: Double, val mass: Double, var xPos: Double, var yPos: Double, val xVel: Double, val yVel: Double, val color: scalafx.scene.paint.Color) extends CelestialBody(name, radius, mass, xPos, yPos, xVel, yVel, color):
+
+
+
