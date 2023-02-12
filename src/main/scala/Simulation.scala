01896af2432242d42c38f6d2704ee90d89f88c53
danielmichaeli <daniel.michaeli@aalto.fi>
2023-02-12 19:30:37 +0200

    added updatePositions() method that updates positions for all planets in the simulation. Composed of earlier aux methods velocityVerlet, updateAcceleration, totalForces.

--- src/main/scala/Simulation.scala
+++ src/main/scala/Simulation.scala
@@ -3,16 +3,19 @@ import scala.collection.mutable.Buffer
 import scala.math.{atan, cos, pow, sin, sqrt}
 
 class Simulation:
-
+  var time = 0
   val celestialBodies = Buffer[CelestialBody]()
-
 
   //reads in a text file and instantiates the celestial bodies
   def parseData(): Unit = ???
 
-
-  //method onTick to update everything (all planets forces, positions, accelerations et.c. Should use above methods)
-  def updateState(): Unit = ???
-  //calculate total force for all bodies
-  //velocity verlet using old and new acceleration
+  //updates all planets accelerations, positions and velocities
+  def updatePositions(): Unit =
+    for body <- celestialBodies do
+      newX = velocityVerlet( body.xPos, body.xVel, body.xAcc, updateAcceleration(body,celestialBodies) )
+      newY = velocityVerlet( body.yPos, body.yVel, body.yAcc, updateAcceleration(body,celestialBodies) )
+      body.xPos = newX(0)
+      body.yPos = newY(0)
+      body.xVel = newX(1)
+      body.yVel = newY(1)
 
