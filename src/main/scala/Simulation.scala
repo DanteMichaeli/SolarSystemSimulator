2d2a0c4ef1842abbd01b6d8b3ab49ab81e32d7db
danielmichaeli <daniel.michaeli@aalto.fi>
2023-02-12 19:11:20 +0200

    added skeletons to simulation file: effectful parseData method and updateState method

--- src/main/scala/Simulation.scala
+++ src/main/scala/Simulation.scala
@@ -1,5 +1,18 @@
 //CLASS FOR THE SIMULATION LOGIG. EACH INSTANTIATION REPRESENTS A SINGLE SIMULATION
+import scala.collection.mutable.Buffer
+import scala.math.{atan, cos, pow, sin, sqrt}
 
-class Simulation {
+class Simulation:
 
-}
+  val celestialBodies = Buffer[CelestialBody]()
+
+
+  //reads in a text file and instantiates the celestial bodies
+  def parseData(): Unit = ???
+
+
+  //method onTick to update everything (all planets forces, positions, accelerations et.c. Should use above methods)
+  def updateState(): Unit = ???
+  //calculate total force for all bodies
+  //velocity verlet using old and new acceleration
+
