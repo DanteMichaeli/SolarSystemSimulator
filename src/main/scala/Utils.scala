5fd347fa2998b5db8e889ef8044035ea0ca4a745
danielmichaeli <daniel.michaeli@aalto.fi>
2023-03-27 06:33:44 +0300

    found a system of sun + four bodies that is somewhat stable

--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -5,13 +5,13 @@ import scala.collection.mutable.Buffer
 
 //PHYSICS CONSTANTS:
 val G = 6.6743*pow(10,-0.5) //m^3 * kg^(-1) * s^(-2)
-val dt: Double = 2 //1 day timestep (?)
+val dt: Double = 0.5 //larger time-step means faster simulation, but less accurate
 
 
 //GUI CONSTANTS
 val GUIwidth: Int = 800
 val GUIheight: Int = 800
-val scalingFactor = 3* 20/696340e3 //adjust based on set sun radius of 65 pixels
+val scalingFactor = 65/696340e30 //adjust based on set sun radius of 65 pixels
 
 //calculates new velocity and position vectors of body based on current position, velocity and acceleration and new acceleration.
 def velocityVerlet(currentPos: Vector2D, currentVel: Vector2D, currentAcc: Vector2D, newAcc: Vector2D): (Vector2D, Vector2D) =
