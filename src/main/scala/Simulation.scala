bbe68be9ebf4e30c2b148d0ca859a401ff2f29ab
danielmichaeli <daniel.michaeli@aalto.fi>
2023-04-09 00:21:12 +0300

    - created Exception Handling for parseData(). Correct syntax but not working (app still launches)

--- src/main/scala/Simulation.scala
+++ src/main/scala/Simulation.scala
@@ -7,30 +7,46 @@ class Simulation:
   var time = 0.00
   val celestialBodies = Buffer[CelestialBody]()
 
-  //reads in a (properly formatted) text file and instantiates the celestial bodies
-
+  //reads in a (properly formatted) text file and instantiates the celestial bodies or throws an exception
   def parseData() =
-    val source = scala.io.Source.fromFile("theSolarSystem.txt")
-    val lines = source.getLines().toList
-    source.close()
+    try
+      val source = scala.io.Source.fromFile("theSolarSystem.txt")
+      val lines = source.getLines().toList
+      source.close()
 
-    var firstLine = true
+        var firstLine = true
+        for line <- lines do
+            if firstLine then
+              time = line.toDouble //time (in seconds) for how long to run the simulation.
+              firstLine = false
+            else
+              val cols = line.split(", ").map(_.trim)
+              if cols(0) == "sun" then try
+                val body = new Sun( cols(1), cols(2).toDouble, cols(3).toDouble, Vector2D(cols(4).toDouble, cols(5).toDouble), Vector2D(0,0), Color.web(cols(6)) )
+                celestialBodies += body
+              catch
+                case nonPositiveRad if cols(2).toDouble <= 0 => throw new IllegalArgumentException("Non-positive radius error - Make sure none of your radii are negative or zero.")
+                case nonPositiveMass if cols(3).toDouble <= 0 => throw new IllegalArgumentException("Non-positive mass error - Make sure none of your masses are negative or zero.")
+                case negativeCoords if cols(4).toDouble < 0 || cols(5).toDouble < 0 => throw new IllegalArgumentException("Negative coordinates error - Make sure none of your initializing coordinates are negative.")
 
-    for line <- lines do
-      if firstLine then
-        time = line.toDouble //time (in seconds) for how long to run the simulation.
-        firstLine = false
-      else
-        val cols = line.split(", ").map(_.trim)
-        if cols(0) == "sun" then
-          val body = new Sun( cols(1), cols(2).toDouble, cols(3).toDouble, Vector2D(cols(4).toDouble, cols(5).toDouble), Vector2D(0,0), Color.web(cols(6)) )
-          celestialBodies += body
-        else if cols(0) == "pla" then
-          val body = new Planet( cols(1), cols(2).toDouble, cols(3).toDouble, Vector2D(cols(4).toDouble, cols(5).toDouble), Vector2D(cols(6).toDouble, cols(7).toDouble), Color.web(cols(8)) )
-          celestialBodies += body
-        else
-          val body = new Satellite( cols(1), cols(2).toDouble, cols(3).toDouble, Vector2D(cols(4).toDouble, cols(5).toDouble), Vector2D(cols(6).toDouble, cols(7).toDouble), Color.web(cols(8)) )
-          celestialBodies += body
+              else if cols(0) == "pla" then try
+                val body = new Planet( cols(1), cols(2).toDouble, cols(3).toDouble, Vector2D(cols(4).toDouble, cols(5).toDouble), Vector2D(cols(6).toDouble, cols(7).toDouble), Color.web(cols(8)) )
+                celestialBodies += body
+              catch
+                case nonPositiveRad if cols(2).toDouble <= 0 => throw new IllegalArgumentException("Non-positive radius error - Make sure none of your radii are negative or zero.")
+                case nonPositiveMass if cols(3).toDouble <= 0 => throw new IllegalArgumentException("Non-positive mass error - Make sure none of your masses are negative or zero.")
+                case negativeCoords if cols(4).toDouble < 0 || cols(5).toDouble < 0 => throw new IllegalArgumentException("Negative coordinates error - Make sure none of your initializing coordinates are negative.")
+
+              else try
+                val body = new Satellite( cols(1), cols(2).toDouble, cols(3).toDouble, Vector2D(cols(4).toDouble, cols(5).toDouble), Vector2D(cols(6).toDouble, cols(7).toDouble), Color.web(cols(8)) )
+                celestialBodies += body
+              catch
+                case nonPositiveRad if cols(2).toDouble <= 0 => throw new IllegalArgumentException("Non-positive radius error - Make sure none of your radii are negative or zero.")
+                case nonPositiveMass if cols(3).toDouble <= 0 => throw new IllegalArgumentException("Non-positive mass error - Make sure none of your masses are negative or zero.")
+                case negativeCoords if cols(4).toDouble < 0 || cols(5).toDouble < 0 => throw new IllegalArgumentException("Negative coordinates error - Make sure none of your initializing coordinates are negative.")
+
+    catch
+      case e: Exception => throw new IllegalArgumentException("File Structure Error - Make sure your input file has the correct structure:\ntime of simulation (s)\nplanet type (3-letter), name, mass (kg), radius (m), x position (px), y position (px), x velocity (m/s), y velocity (m/s), color (hex code)\n.\n.\n.")
 
 
 
